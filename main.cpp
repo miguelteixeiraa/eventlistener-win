e75bdba692def548adbce4a475614c6fb19a750b
Miguel Teixeira <31167673+miguelteixeiraa@users.noreply.github.com>
2020-07-02 20:22:11 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,17 +2,18 @@
 #include <wineventlistener.h>
 #include <QList>
 #include <QString>
-#include <QTextCodec>
-int main(int argc, char *argv[])
-{
-    QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
+
+int main(int argc, char *argv[]){
+    
     WinEventListener w;
     QList<QString> q{
         "UIA_ToolTipOpenedEventId",
         "UIA_ToolTipClosedEventId"
     };
+    
     w.addEventsToIdentify(q);
     w.listenerStart();
+    
     QCoreApplication a(argc, argv);
     return a.exec();
 }
