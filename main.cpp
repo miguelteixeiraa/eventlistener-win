ccf6c15b17dcd4f94e2548972d5ba0ec157ffb52
Miguel Teixeira <miguel.teixeira@poli.ufrj.br>
2020-07-03 20:56:01 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
-#include <QCoreApplication>
-#include <wineventlistener.h>
 #include <QList>
 #include <QString>
 #include <clocale>
+
+#include <wineventlistener.h>
 
 
 int main(int argc, char *argv[])
 {
     setlocale(LC_ALL, "portuguese-brazilian");
+    
     QList<QString> event_list{
         "UIA_ToolTipOpenedEventId",
         "UIA_ToolTipClosedEventId"
@@ -16,8 +17,6 @@ int main(int argc, char *argv[])
     WinEventListener w;
     w.addEventsToIdentify(event_list);
     w.listenerStart();
-
-    QCoreApplication a(argc, argv);
 
     return a.exec();
 }
